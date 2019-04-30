61f3510f9429217b22c01905d2e72aa533e373bc
PYN <yxp7531540@gmail.com>
2019-04-30 15:27:44 -0500

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(recipe_id: @recipe.id)
+    @recipe.ingredients.build(recipe_id: @recipe.id)
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+
+    redirect_to @recipe
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:quantity, :name])
+
   end
 end
